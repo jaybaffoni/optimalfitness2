352bafdbc3af2c19630fcca81bac45ef5dcd037c
James Baffoni <jay@baffoni5.com>
2021-06-17 13:51:38 -0400

    beach house panel updates

--- src/app/pages/locations/panels/beach-house-panel/beach-house-panel.component.ts
+++ src/app/pages/locations/panels/beach-house-panel/beach-house-panel.component.ts
@@ -10,8 +10,6 @@ export class BeachHousePanelComponent implements OnInit {
   public images = [
     {path: './assets/images/beach_house1.jpg'},
     {path: './assets/images/natick beach house.JPG'},
-    // {path: './assets/images/bh Cam Bear crawl DB drag.HEIC'},
-    // {path: './assets/images/Beach House Entrance.HEIC'},
     {path: './assets/images/beachhouse4.jpeg'},
     {path: './assets/images/Beach House Courts 2.jpg'},
   ];
